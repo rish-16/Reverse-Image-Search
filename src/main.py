1ae84470238f1b8f3e91f194980b7f1861c99628
rish-16 <mail.rishabh.anand@gmail.com>
2019-11-11 00:05:22 +0800

    Added model code

--- src/main.py
+++ src/main.py
@@ -2,4 +2,5 @@ import tensorflow as tf
 import numpy as np
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Dropout, LSTM
+from tensorflow.keras.layers import Dense, Conv2D, Dropout, LSTM, Embedding, Flatten
+from tensorflow.keras.text.preprocessing import pad_sequences
